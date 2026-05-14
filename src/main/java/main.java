ad633793698096eb8d3bca3ac296e01f509c5350
vinicius-rodrigues-cordeiro <vrc_crv@outlook.com.br>
2026-05-14 20:00:00 -0300

    chore: remove main.java do repositório

--- src/main/java/main.java
+++
@@ -1,9 +0,0 @@
-import static org.example.demo.service.AutenticacaoService.gerarHash;
-
-public class main {
-    public static void main(String[] args) {
-        String senha = "@Alfa@";
-        String hash = gerarHash(senha);
-        System.out.println("Hash gerado: " + hash);
-    }
-}
