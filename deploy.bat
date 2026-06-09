f27b78157cf9308fcc5a4af9b829951f313c5d87
vinicius-rodrigues-cordeiro <vrc_crv@outlook.com.br>
2026-06-09 20:40:33 -0300

    fix: deploy.bat corrigido com JAVA_HOME e startup em janela separada

--- deploy.bat
+++ deploy.bat
@@ -11,9 +11,9 @@ set PATH=%JAVA_HOME%\bin;%PATH%
 
 echo.
 echo [1/4] Gerando WAR...
-"C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2025.2.1\plugins\maven\lib\maven3\bin\mvn.cmd" clean package -f pom.xml
+"C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2025.2.1\plugins\maven\lib\maven3\bin\mvn.cmd" clean package -f "C:\Users\milar\Downloads\Projeto\pom.xml"
 
-if errorlevel 1 (
+if %ERRORLEVEL% NEQ 0 (
     echo.
     echo ERRO: Build falhou!
     pause
@@ -21,18 +21,21 @@ if errorlevel 1 (
 )
 
 echo.
-echo [2/4] Parando Tomcat (se estiver rodando)...
-taskkill /F /IM "tomcat*.exe" >nul 2>&1
-taskkill /F /FI "WINDOWTITLE eq Tomcat*" >nul 2>&1
-timeout /t 2 /nobreak >nul
+echo [2/4] Parando Tomcat se estiver rodando...
+taskkill /F /IM "tomcat10.exe" >nul 2>&1
+taskkill /F /IM "java.exe" >nul 2>&1
+timeout /t 3 /nobreak >nul
 
 echo.
 echo [3/4] Copiando WAR...
 copy /Y "C:\Users\milar\Downloads\Projeto\target\demo-1.0-SNAPSHOT.war" "C:\Users\milar\Downloads\apache-tomcat-10.1.54\webapps\demo.war"
+echo WAR copiado com sucesso!
 
 echo.
 echo [4/4] Iniciando Tomcat...
-start "Tomcat" /B cmd /c "set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-21.0.5.11-hotspot && C:\Users\milar\Downloads\apache-tomcat-10.1.54\bin\startup.bat"
+set CATALINA_HOME=C:\Users\milar\Downloads\apache-tomcat-10.1.54
+set CATALINA_BASE=C:\Users\milar\Downloads\apache-tomcat-10.1.54
+start "Tomcat Library Digital" "C:\Users\milar\Downloads\apache-tomcat-10.1.54\bin\startup.bat"
 
 timeout /t 5 /nobreak >nul
 
